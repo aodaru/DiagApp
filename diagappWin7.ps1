4ac45c688889bec858e7086ef1a8823afd04189d
Adal Michael García <aodarug@gmail.com>
2022-04-09 11:48:17 -0500

    Modificacion de los comandos de powershell

--- diagappWin7.ps1
+++ diagappWin7.ps1
@@ -1,19 +1,25 @@
-New-Variable -Name PathUser -Value $env:USERPROFILE
-Write-OutPut ========================================================================================= > $PathUser\Desktop\diag.txt
-wmic os get Caption >> $PathUser\Desktop\diag.txt
-Write-Output `n >> $PathUser\Desktop\diag.txt
-Write-OutPut ========================================================================================= >> $PathUser\Desktop\diag.txt
-wmic os get SystemDrive >> $PathUser\Desktop\diag.txt
-Write-OutPut `n >> $PathUser\Desktop\diag.txt
-Write-OutPut ========================================================================================= >> $PathUser\Desktop\diag.txt
-wmic diskdrive get status >> $PathUser\Desktop\diag.txt
-Write-OutPut `n >> $PathUser\Desktop\diag.txt
-Write-OutPut ========================================================================================= >> $PathUser\Desktop\diag.txt
-wmic /namespace:\\root\wmi path MSStorageDriver_FailurePredictStatus >> $PathUser\Desktop\diag.txt
-Write-OutPut `n >> $PathUser\Desktop\diag.txt
-Write-OutPut ========================================================================================= >> $PathUser\Desktop\diag.txt
-chkdsk c: /scan >> $PathUser\Desktop\diag.txt
-Write-OutPut `n >> $PathUser\Desktop\diag.txt
-Write-OutPut ========================================================================================= >> $PathUser\Desktop\diag.txt
-Write-OutPut `n >> $PathUser\Desktop\diag.txt
-defrag c: /A >> $PathUser\Desktop\diag.txt
+New-Variable -Name PathRoot -Value $env:WinDir.Substring(0,3)
+Mkdir $PathRoot\DiagTest
+Write-OutPut ========================================================================================= > $PathRoot\DiagTest\diag.txt
+wmic os get Caption >> $PathRoot\DiagTest\diag.txt
+Write-Output `n >> $PathRoot\DiagTest\diag.txt
+Write-OutPut ========================================================================================= >> $PathRoot\DiagTest\diag.txt
+wmic os get SystemDrive >> $PathRoot\DiagTest\diag.txt
+Write-OutPut `n >> $PathRoot\DiagTest\diag.txt
+Write-OutPut ========================================================================================= >> $PathRoot\DiagTest\diag.txt
+wmic diskdrive get status >> $PathRoot\DiagTest\diag.txt
+Write-OutPut `n >> $PathRoot\DiagTest\diag.txt
+Write-OutPut ========================================================================================= >> $PathRoot\DiagTest\diag.txt
+wmic /namespace:\\root\wmi path MSStorageDriver_FailurePredictStatus >> $PathRoot\DiagTest\diag.txt
+Write-OutPut `n >> $PathRoot\DiagTest\diag.txt
+Write-OutPut ========================================================================================= >> $PathRoot\DiagTest\diag.txt
+chkdsk c: /scan >> $PathRoot\DiagTest\diag.txt
+Write-OutPut `n >> $PathRoot\DiagTest\diag.txt
+Write-OutPut ========================================================================================= >> $PathRoot\DiagTest\diag.txt
+Write-OutPut `n >> $PathRoot\DiagTest\dia.txt
+defrag c: /A >> $PathRoot\DiagTest\diag.txt
+Get-Content $PathRoot\DiagTest\diag.txt
+Remove-Item $PathRoot\DiagTest
+Write-Output "Ejecucion terminanda"
+
+# iex ((New-Object System.Net.WebClient).DownloadString('https://bit.ly/3xcdPCD'))
